a33c43eee1f428ef980495bf08adc025adc0c5de
harshikak7 <harshikakolekar184@gmail.com>
2026-05-07 21:39:44 +0530

    update contains duplicate solution

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,10 +1,10 @@
 var containsDuplicate=function(nums){
     nums.sort((a,b)=>a-b);
 
-    for(let i =0;i<nums.length;i++){
-        if(nums[i]===nums[i-1]){
+    for(let i=0;i<nums.lenght;i++){
+        if(nums[i]===nums[i+1]){
             return true
         }
     }
-    return false
+    return false;
 }
