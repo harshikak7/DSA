92c2da67a62317b298e51e1130eb846a3a689cf4
Harshika Kolekar <166911366+harshikak7@users.noreply.github.com>
2026-05-07 21:45:12 +0530

    Fix sorting comment and typo in containsDuplicate function
    Fixed sorting comment and corrected typo in loop condition.

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,5 +1,5 @@
 var containsDuplicate=function(nums){
-    nums.sort((a,b)=>a-b);
+    nums.sort((a,b)=>a-b); //JS Sort uses string so convert to number sort
 
     for(let i=0;i<nums.lenght;i++){
         if(nums[i]===nums[i+1]){
