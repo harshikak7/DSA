0e216b614e773fc65ff65f7123a5098a0a84c423
harshikak7 <harshikakolekar184@gmail.com>
2026-05-14 20:45:31 +0530

    product of array except self optimal solution

--- product_of_array_except_self.js
+++ product_of_array_except_self.js
@@ -1,24 +1,42 @@
 var productExceptSelf=function(nums){
-    let n=nums.length
-    let left=new Array(n)
-    let right=new Array(n)
-    let res=new Array(n)
+    
+    // Predfix + Suffix Optimal
+    const n=nums.length
+    const res=new Array(n).fill(1)
 
-    left[0]=1
-    right[n-1]=1
-
-    for(let i=1; i<n ; i++){
-        left[i]=left[i-1]*nums[i-1]
+    for(let i=1;i<n;i++){
+        res[i]=res[i-1]*nums[i-1]
     }
 
-    for(let i=n-2;i>=0;i--){
-        right[i]=right[i+1]*nums[i+1]
-    }
-
-    for(let i=0;i<n;i++){
-        res[i]=left[i]*right[i]
+    let postfix=1
+    for(let i=n-1;i>=0;i--){
+        res[i]*=postfix
+        postfix*=nums[i]
     }
     return res
+
+
+    // Prefix + Suffix Sum 
+    // let n=nums.length
+    // let left=new Array(n)
+    // let right=new Array(n)
+    // let res=new Array(n)
+
+    // left[0]=1
+    // right[n-1]=1
+
+    // for(let i=1; i<n ; i++){
+    //     left[i]=left[i-1]*nums[i-1]
+    // }
+
+    // for(let i=n-2;i>=0;i--){
+    //     right[i]=right[i+1]*nums[i+1]
+    // }
+
+    // for(let i=0;i<n;i++){
+    //     res[i]=left[i]*right[i]
+    // }
+    // return res
 }
 
 console.log(productExceptSelf([1,2,3,4]))
