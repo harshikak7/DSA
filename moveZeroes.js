87156300e836295d4719608d58cc98d5bec3f3e9
harshikak7 <harshikakolekar184@gmail.com>
2026-05-19 21:25:17 +0530

    Move Zeroes

---
+++ moveZeroes.js
@@ -0,0 +1,11 @@
+var moveZeroes=function(nums){
+    let left=0
+    for(let right=0;right<nums.length;right++){
+        if(nums[right]!=0){
+            [nums[left],nums[right]]=[nums[right],nums[left]]
+            left++
+        }
+    }
+    return nums
+}
+console.log(moveZeroes([1,0,3,0,3,12]))
