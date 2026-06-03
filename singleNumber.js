023bb1d2e9ca96ba39b6f372392e563c7f06ce91
harshikak7 <harshikakolekar184@gmail.com>
2026-06-03 22:57:13 +0530

    single number problem optimal

--- singleNumber.js
+++ singleNumber.js
@@ -1,13 +1,25 @@
-var singleNumber=function(nums){
-    let map={}    
-    for(let num of nums){
-        map[num]=(map[nums] || 0)+ 1
-    }
-    for(let key in map){
-        if(map[key]===1){
-            return Number(key)
-        }
-    }
-}
+//Using Hashset 
+// var singleNumber=function(nums){
+//     let map={}    
+//     for(let num of nums){
+//         map[num]=(map[nums]||0)+1
+//     }
+//     for(let key in map){
+//         if(map[key]===1){
+//             return Number(key)
+//         }
+//     }
+// }
 
+// console.log(singleNumber([2,2,1]))
+
+
+// Using XOR Bit Manipulation
+var singleNumber=function(nums){
+    let result=0
+    for(num of nums){
+        result^=num
+    }
+    return result
+}
 console.log(singleNumber([2,2,1]))
